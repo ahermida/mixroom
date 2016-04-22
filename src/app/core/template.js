a0d238ecb05363e407541f3b289a99986c9a3fc4
Albert Hermida <alberthermida@gmail.com>
2016-04-22 00:57:14 -0400

    added template data for posts

--- src/app/core/template.js
+++ src/app/core/template.js
@@ -3,34 +3,68 @@
  */
 
 import { threadLength } from '../ajax/thread.js';
+import store from './store.js';
 
-//creates a post's html given we have the data
+// type Post struct {
+// 	Id          bson.ObjectId   `bson:"_id,omitempty" json:"-"`
+//   SId         string          `bson:"id,omitempty" json:"id"`
+//   Thread      bson.ObjectId   `bson:"thread,omitempty" json:"-"`
+//   Created     time.Time       `bson:"created" json:"created"`
+// 	Author      string          `bson:"author" json:"author"`
+//   AuthorId    bson.ObjectId   `bson:"authorId,omitempty" json:"-"`
+//   Replies     []bson.ObjectId `bson:"replies" json:"replies"`
+//   ResponseTo  []bson.ObjectId `bson:"responseTo" json:"responseTo"`
+//   Content     string          `bson:"content,omitempty" json:"content"`
+//   ContentType string          `bson:"contentType,omitempty" json:"contentType"`
+//   Body        string          `bson:"body" json:"body"`
+// }
+//creates a post's html
 export function generatePost(post) {
-  //For the Group View
-  // type Mthread struct {
-  // 	Id       bson.ObjectId `bson:"_id,omitempty" json:"-"`
-  //   SId      string        `bson:"id" json:"id"`
-  //   Created  time.Time     `bson:"created" json:"created"`
-  //   Thread   bson.ObjectId `bson:"thread" json:"-"`
-  //   ThreadId string        `bson:"threadId" json:"thread"`
-  //   Head     *Post         `bson:"head" json:"head"`
-  //   Group    string        `bson:"group" json:"group"`
-  // }
-  //
-  // type Post struct {
-  // Id          bson.ObjectId   `bson:"_id,omitempty" json:"-"`
-  // SId         string          `bson:"id,omitempty" json:"id"`
-  // Thread      bson.ObjectId   `bson:"thread,omitempty" json:"-"`
-  // Created     time.Time       `bson:"created" json:"created"`
-  // Author      string          `bson:"author" json:"author"`
-  // AuthorId    bson.ObjectId   `bson:"authorId,omitempty" json:"-"`
-  // Replies     []bson.ObjectId `bson:"replies" json:"replies"`
-  // ResponseTo  []bson.ObjectId `bson:"responseTo" json:"responseTo"`
-  // Content     string          `bson:"content,omitempty" json:"content"`
-  // ContentType string          `bson:"contentType,omitempty" json:"contentType"`
-  // Body        string          `bson:"body" json:"body"`
-  // }
+  const postID = post.id;
 
+  let headpost = `
+    <div id="${postID}" class="HeadPost">
+      <header class="Header">
+      ${generateHeader(thread.group, thread.author, timestamp, true)}
+      </header>
+      <div class="Content">
+      ${generateContent(post.content, post.contentType)}
+      </div>
+      <div class="Body">
+      ${generateBody(post.body)}
+      </div>
+      <footer class="Footer">
+      ${generatePostHeadFooter(threadID, store.user.anonymous)}
+      </footer>
+    </div>
+  `
+  return headpost;
+}
+
+//creates a head post's html given we have the data
+export function generateHeadPost(thread) {
+  const post = thread.head;
+  const threadID = thread.thread;
+  const timestamp = thread.created;
+  const postID = post.id;
+
+  let headpost = `
+    <div id="${threadID}" class="HeadPost">
+      <header class="Header">
+      ${generatePostHeadHeader(thread.group, thread.author, timestamp, true)}
+      </header>
+      <div class="Content">
+      ${generateContent(post.content, post.contentType)}
+      </div>
+      <div class="Body">
+      ${generateBody(post.body)}
+      </div>
+      <footer class="Footer">
+      ${generatePostHeadFooter(threadID, store.user.anonymous)}
+      </footer>
+    </div>
+  `
+  return headpost;
 }
 
 function generateTimestamp(timestamp) {
@@ -50,19 +84,39 @@ function generateTimestamp(timestamp) {
   return `${hour}:${minutesString}${ampm} ${fullyr}`;
 }
 
-function generateHead(group, author, authorId, replies, created) {
-
+//generate the header for a post --> don't show replies if head
+function generatePostHeadHeader(group, author, created, head) {
   //title for each of the posts, replies should be overflow-x
   return `
-    <div class="Head-title">
-      <span class="Head-title-author">${author}</span>
-      -
-      <a class="Head-title-group">${group}</a>
-      -
-      <span class="Head-title-created">${generateTimestamp(created)}</span>
-      -
-      <span class="Head-title-replies">
-      ${replies.map(item => `<span class="Head-title-rep">${item}</span>`).join('')}
+    <div class="Head-content">
+      <span class="Head-left">
+        <span class="Head-author">${author}</span>
+        -
+        <a class="Head-group">${group}</a>
+        -
+        <span class="Head-created">${generateTimestamp(created)}</span>
+      </span>
+      <span class="Head-rm">
+        <span class="icon-down-open-big"></span>
+      </span>
+    </div>
+  `;
+}
+
+//generate the header for a post --> don't show replies if head
+function generatePostHeader(group, author, created, head) {
+  //title for each of the posts, replies should be overflow-x
+  return `
+    <div class="Head-content">
+      <span class="Head-left">
+        <span class="Head-author">${author}</span>
+        -
+        <span class="Head-contentType">${post.contentType || 'text'}</span>
+        -
+        <span class="Head-created">${generateTimestamp(created)}</span>
+      </span>
+      <span class="Head-rm">
+        <span class="icon-down-open-big"></span>
       </span>
     </div>
   `;
@@ -92,13 +146,13 @@ function generateContent(content, contentType) {
 
 //handle body of post
 function generateBody(str) {
-  return `<div class="Body">${parser(str)}</div>`
+  return `<div class="Body-content">${parser(str)}</div>`
 }
 
-//handle footer of post
-function generateFooter(thread) {
+//gets thread's length via ajax
+function getThreadLength(thread) {
   let length;
-  (async () => {
+  (async (thread) => {
     try {
       let res = await threadLength(thread);
       let resp = await res.json();
@@ -107,10 +161,52 @@ function generateFooter(thread) {
       console.log(e);
     }
   })(thread);
+  return length;
+}
+
+//handle footer of thread post (head)
+function generatePostHeadFooter(thread, loggedIn) {
+  let length = getThreadLength(thread);
   let footer = `
   <div class="Footer-content">
-    ${}
+    <span class="Footer-left">
+      <span class="icon-comment Footer-left-icon"></span>
+      <span class="Footer-left-size">${length} posts</span>
+    </span>
+    <span class="Footer-right">
+      ${loggedIn ? `<span class="Footer-right-save">save</span>` : ''}
+      <span class="Footer-right-reply">
+        <span class="icon-reply"></span>
+        reply
+      </span>
+      <span class="Footer-open">
+        open
+      </span>
+    </span>
   </div>
   `;
+  return footer;
+}
 
+//handle footer of thread post (head)
+function generatePostFooter(replies) {
+  //might make calls in here later -> that's why it's a function
+  let footer = `
+  <div class="Footer-content">
+    <span class="Footer-left">
+      <span class="icon-comment Footer-left-icon"></span>
+      <span class="Footer-left-size">${replies} replies</span>
+    </span>
+    <span class="Footer-right">
+      <span class="report">
+        report
+      </span>
+      <span class="Footer-right-reply">
+        <span class="icon-reply"></span>
+        reply
+      </span>
+    </span>
+  </div>
+  `;
+  return footer;
 }
